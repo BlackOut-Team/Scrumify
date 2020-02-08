c2baf9d1f8ff170e75fe7946739bba1a0093f975
Amira Doghri <doghriamira@gmail.com>
2020-02-09 00:40:35 +0100

    sign in / register

--- src/MainBundle/Controller/DefaultController.php
+++ src/MainBundle/Controller/DefaultController.php
@@ -14,8 +14,16 @@ class DefaultController extends Controller
     {
         return $this->render('@Main/Default/index.html.twig');
     }
+    /**
+     * @Route("/register")
+     */
+    public function loginAction()
+    {
+        return $this->render('@Main/Security/login.html.twig');
+    }
     public function registerAction()
     {
-        return $this->render('@Main/Default/register.html.twig');
+        return $this->render('@Main/Registration/register.html.twig');
     }
+
 }

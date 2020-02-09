24f6eef84ee4793af175ba65975c3a3cf7966251
Hidaya <hidaya.mcharek@esprit.tn>
2020-02-09 12:40:47 +0100

    config

---
+++ src/MainBundle/Form/UserType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace MainBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use MainBundle\Form\UserType as BaseRegistrationFormType;
+class UserType extends AbstractType
+{
+/**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'MainBundle\Entity\User'
+        ));
+    }
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('name');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'mainbundle_user';
+    }
+
+
+    public function getParent()
+    {
+        return BaseRegistrationFormType::class;
+    }
+}

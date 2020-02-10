015abeccf93524b172d81392c23a7ad0e4cb8235
rami <ramijaiem@gmail.com>
2020-02-10 18:02:04 +0100

    forum and activities

--- src/UserstoryBundle/Controller/DefaultController.php
+++ src/UserstoryBundle/Controller/DefaultController.php
@@ -8,6 +8,6 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('UserstoryBundle:Default:index.html.twig');
+        return $this->render('@Userstory/Default/index.html.twig');
     }
 }

9a84b0ec7bdd19bc705b2c2b62c26823aff115b9
Amira Doghri <doghriamira@gmail.com>
2020-02-12 11:42:35 +0100

    Back office project

--- src/SprintBundle/Controller/DefaultController.php
+++ src/SprintBundle/Controller/DefaultController.php
@@ -22,14 +22,14 @@ class DefaultController extends Controller
         if ($f->isSubmitted() && $f->isValid()) {
 
             $em = $this->getDoctrine()->getManager();
-            //$s->setProject($projet);
+            $s->setProject($projet);
             $s->setEtat(1);
             $s->setCreated(new \DateTime('now'));
             $em->persist($s);
 
             $em->flush($s);
 
-            return $this->redirectToRoute('addSprint');
+            return $this->redirectToRoute('addProject');
 
         }
         return $this->render('@Sprint/Default/index.html.twig',array(
@@ -51,4 +51,18 @@ class DefaultController extends Controller
         return $this->redirectToRoute('addSprint');
 
     }
+    public function editSAction(Request $request, Sprint $sprint){
+        $editForm=$this->createForm('SprintBundle\Form\SprintType',$sprint);
+        $editForm->handleRequest($request);
+
+        if($editForm->isSubmitted() && $editForm->isValid())
+        {
+            $this->getDoctrine()->getManager()->flush();
+            return $this->redirectToRoute('Sprint_homepage');
+        }
+        return $this->render('@Sprint/Default/editS.html.twig', array(
+            'edit_form' => $editForm->createView()
+        ));
+    }
+
 }

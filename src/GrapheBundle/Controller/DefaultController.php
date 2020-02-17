c1714253656daa48465c9abd1b734f5fd50897b6
Hidaya <hidaya.mcharek@esprit.tn>
2020-02-17 18:21:29 +0100

    Google Charts part 2

--- src/GrapheBundle/Controller/DefaultController.php
+++ src/GrapheBundle/Controller/DefaultController.php
@@ -8,6 +8,25 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('GrapheBundle:Default:index.html.twig');
+        $pieChart = new PieChart();
+        $pieChart->getData()->setArrayToDataTable(
+            [['Task', 'Hours per Day'],
+                ['Work',     11],
+                ['Eat',      2],
+                ['Commute',  2],
+                ['Watch TV', 2],
+                ['Sleep',    7]
+            ]
+        );
+        $pieChart->getOptions()->setTitle('My Daily Activities');
+        $pieChart->getOptions()->setHeight(500);
+        $pieChart->getOptions()->setWidth(900);
+        $pieChart->getOptions()->getTitleTextStyle()->setBold(true);
+        $pieChart->getOptions()->getTitleTextStyle()->setColor('#009900');
+        $pieChart->getOptions()->getTitleTextStyle()->setItalic(true);
+        $pieChart->getOptions()->getTitleTextStyle()->setFontName('Arial');
+        $pieChart->getOptions()->getTitleTextStyle()->setFontSize(20);
+
+        return $this->render('AppBundle::index.html.twig', array('piechart' => $pieChart));
     }
 }

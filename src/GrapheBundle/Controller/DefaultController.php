8c15ee15576a4e21cfd887c1a5c1205c5d997901
Hidaya <hidaya.mcharek@esprit.tn>
2020-02-17 18:52:33 +0100

    google charte part 3

--- src/GrapheBundle/Controller/DefaultController.php
+++ src/GrapheBundle/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace GrapheBundle\Controller;
 
+use CMEN\GoogleChartsBundle\GoogleCharts\Charts\PieChart;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
@@ -10,15 +11,14 @@ class DefaultController extends Controller
     {
         $pieChart = new PieChart();
         $pieChart->getData()->setArrayToDataTable(
-            [['Task', 'Hours per Day'],
-                ['Work',     11],
-                ['Eat',      2],
-                ['Commute',  2],
-                ['Watch TV', 2],
-                ['Sleep',    7]
+            [['Task', 'number'],
+                ['Done',     11],
+                ['Doing',      2],
+                ['To Do',  2],
+                ['Block', 5],
             ]
         );
-        $pieChart->getOptions()->setTitle('My Daily Activities');
+        $pieChart->getOptions()->setTitle('Daily Activities');
         $pieChart->getOptions()->setHeight(500);
         $pieChart->getOptions()->setWidth(900);
         $pieChart->getOptions()->getTitleTextStyle()->setBold(true);
@@ -27,6 +27,6 @@ class DefaultController extends Controller
         $pieChart->getOptions()->getTitleTextStyle()->setFontName('Arial');
         $pieChart->getOptions()->getTitleTextStyle()->setFontSize(20);
 
-        return $this->render('AppBundle::index.html.twig', array('piechart' => $pieChart));
+        return $this->render('@Tasks/Tasks/Daily.html.twig', array('piechart' => $pieChart));
     }
 }

c9a0c7eccc2bb466493c63e4e1fdfc7c1c17aed2
Rekik Iheb <60516198+iheb-rekik@users.noreply.github.com>
2020-02-22 18:58:25 +0100

    bundle pdf intégration

---
+++ src/TeamBundle/Controller/PdfController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace TeamBundle\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+class PdfController extends Controller
+{
+
+    public function indexAction()
+    {
+
+        $snappy = $this->get('knp_snappy.pdf');
+        $filename = 'Team';
+
+        // use absolute path !
+        $pageUrl = $this->generateUrl('affiche_team1', array(), UrlGeneratorInterface::ABSOLUTE_URL);
+
+        return new Response(
+            $snappy->getOutput($pageUrl),
+            200,
+            array(
+                'Content-Type'          => 'application/pdf',
+                'Content-Disposition'   => 'inline; filename="'.$filename.'.pdf"'
+            )
+        );
+    }
+
+
+
+}
+
--- src/TeamBundle/Controller/TeamController.php
+++ src/TeamBundle/Controller/TeamController.php
@@ -79,6 +79,33 @@ class TeamController extends Controller
         $con = $this -> getDoctrine()->getRepository('TeamBundle:team')->findAll();
         return $this->render('@Team/team/index.html.twig',array('con'=> $con,"form" => $form->createView()));
     }
+    public function affBAction(Request $request)
+    {
+        $p= new team();
+        $form = $this->createFormBuilder($p)
+
+            ->add('name', TextType::class, array('attr' => array('class' => 'form-control','required' => true),'label' => "name"))
+
+
+            ->add('Ajouter', SubmitType::class, array( 'attr' => array('class' => 'template-btn', )))
+
+            ->getForm();
+        $form->handleRequest($request);
+        if ($form->isSubmitted()&&$form->isValid()) {
+
+            $p->setCreated(new \DateTime('now'));
+            $p->setUpdated(new \DateTime('now'));
+            $p->setInd(0);
+            $p->setEtat(0);
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($p);
+
+            $em->flush();
+            return $this->redirectToRoute("affiche_team");
+        }
+        $con = $this -> getDoctrine()->getRepository('TeamBundle:team')->findAll();
+        return $this->render('@Team/team/test.html.twig',array('con'=> $con,"form" => $form->createView()));
+    }
     public function  editAction(Request $request,$id)
     {
         {
@@ -160,6 +187,8 @@ class TeamController extends Controller
 
         ));
 }
+
+
 
     public function desarAction(Request $request, $id){
 

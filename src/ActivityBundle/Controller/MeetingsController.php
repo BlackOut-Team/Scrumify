ac851594acc629ab5783e4dfaf4ada1e4a91c897
sirine-sammoud <sirine.sammoud98@gmail.com>
2020-02-25 22:40:36 +0100

    fix chat bugs

--- src/ActivityBundle/Controller/MeetingsController.php
+++ src/ActivityBundle/Controller/MeetingsController.php
@@ -5,8 +5,12 @@ namespace ActivityBundle\Controller;
 use ActivityBundle\Entity\Activity;
 use ActivityBundle\Entity\Meetings;
 use ActivityBundle\Form\MeetingsType;
+use Doctrine\ORM\EntityRepository;
 use ScrumBundle\Entity\Projet;
 use SprintBundle\Entity\Sprint;
+use TeamBundle\Entity\team;
+use TeamBundle\Entity\team_user;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Twilio\Rest\Client;
@@ -19,31 +23,59 @@ class MeetingsController extends Controller
     function AfficheMettingAction($id,Request $request){
 
         $em = $this->getDoctrine()->getManager();
+
+        //select project
         $project=$this->getDoctrine()
             ->getRepository(Projet::class)
             ->find($id);
+
+        //select sprints mil base de donnee(array)
         $sprints = $this->getDoctrine()
             ->getRepository(Sprint::class)
             ->findBy(['project' => $project]);
+
+        //select team
+        $team = $this->getDoctrine()
+            ->getRepository(team::class)
+            ->find($project->getTeam());
+
+        //select team members
+        $teamMembers = $this->getDoctrine()
+            ->getRepository(team_user::class)
+            ->findBy(['teamId' => $team->getId()]);
+
+        //display meeting
         $meetingadd = new Meetings();
         $meeting=$this->getDoctrine()
             ->getRepository(Meetings::class)
             ->findAll();
+
+        //display activities
         $Activities=$this->getDoctrine()
             ->getRepository(Activity::class)
             ->findBy(['viewed' => 1]);
+
+        //display new activities
         $NewActivities=$this->getDoctrine()
             ->getRepository(Activity::class)
             ->findBy(['viewed'=>0]);
+
+        //create form
         $ajouterFrorm = $this->createForm(MeetingsType::class, $meetingadd);
         $ajouterFrorm->handleRequest($request);
 
         if ($ajouterFrorm->isSubmitted() && $ajouterFrorm->isValid()) {
+
+            //create meeting
             $em->persist($meetingadd);
             $em->flush();
 
-            $notif =$this->get('mgilet.notification')->createNotification('Notification subject', "you have a meeting (". $meetingadd->getName() .") in " . $meetingadd->getPlace() ." at ". $meetingadd->getMeetingDate()->format('Y-m-d H:i'), 'https://google.fr/');
-            $this->get('mgilet.notification')->addNotification(array($this->getUser()), $notif, true);
+
+            //appel lel service mta bundle extern -> createNotification
+            $notif =$this->get('mgilet.notification')->createNotification('Meeting !', "you have a meeting (". $meetingadd->getName() .") in " . $meetingadd->getPlace() ." at ". $meetingadd->getMeetingDate()->format('Y-m-d H:i'), '');
+            foreach ( $teamMembers as $teamMember){
+                $this->get('mgilet.notification')->addNotification(array($teamMember->getUserId()), $notif, true);
+            }
             //send sms when adding meeting (extern bundle)
             //$sid = "ACabcbc80d4384e812cad9003a0e0572df"; // Your Account SID from www.twilio.com/console
             //$token = "88420746fc00b5b421774e629777891a"; // Your Auth Token from www.twilio.com/console
@@ -59,18 +91,22 @@ class MeetingsController extends Controller
 
         }
         return $this->render('@Activity/Default/activity.html.twig',
-            array('activities'=>$Activities,'m'=>$meeting, "f"=>$ajouterFrorm->createView(), 'newActivities'=>$NewActivities, 'sprints'=> $sprints ));
+            array('activities'=>$Activities,'m'=>$meeting, "f"=>$ajouterFrorm->createView(), 'newActivities'=>$NewActivities, 'sprints'=> $sprints, 'id' => $id, 'team' =>$teamMembers));
     }
-    function SupprimerAction($id){
+
+
+
+
+    function SupprimerAction($id,$project_id){
         $em=$this->getDoctrine()->getManager();
         $meeting=$em->getRepository(Meetings::class)
             ->find($id);
         $em->remove($meeting);
         $em->flush();
-        return $this->redirectToRoute('affichermeeting');
+        return $this->redirectToRoute('affichermeeting',['id' =>$project_id ]);
 
     }
-    function modifierAction($id, Request $request)
+    function modifierAction($id,$project_id , Request $request)
     {
         $em = $this->getDoctrine()->getManager();
         $meeting = $em->getRepository(Meetings::class)
@@ -82,7 +118,7 @@ class MeetingsController extends Controller
         if ($Form->isSubmitted()) {
             $em = $this->getDoctrine()->getManager();
             $em->flush();
-            return $this->redirectToRoute('affichermeeting');
+            return $this->redirectToRoute('affichermeeting',['id' =>$project_id ]);
 
         }
         return $this->render('@Activity/Default/modifierMeeting.html.twig',
